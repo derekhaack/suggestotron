bb2aa3590b448ce46a86e95dc3303840d4a19eed
Derek Haack <derek.haack@sumcumo.com>
2017-03-13 16:49:47 +0100

    Editin route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   resources :topics do
   	member do
   		post 'upvote'
+
+        get '/topic' => 'topic#new'
   	end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
